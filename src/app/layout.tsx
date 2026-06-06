c2ac0112792037b2e2f81ac052adc326212cc18b
Tomi Paajanen <tomi.paajanen@edu.karelia.fi>
2026-06-06 19:13:41 +0900

    Use Next.js optimized Vercel Analytics import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Kaisei_Decol } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,9 +36,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${kaiseiDecol.variable} antialiased`}
       >
         {children}
-        <Script 
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+        <Analytics />
+        <Script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossOrigin="anonymous"
             strategy="beforeInteractive"
             />
